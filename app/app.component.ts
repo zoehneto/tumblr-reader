785c706e9a13cac41424ee38b279c8e154432096
Tom Z?hner <tom.zoehner@gmail.com>
2016-05-08 21:42:19 +0200

    Added placeholder sidebar component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,17 +3,24 @@ import {TumblrService} from "./tumblr.service";
 import {ConfigService} from "./config.service";
 import {Blog} from "./data.types";
 import {Post} from "./data.types";
+import {SidebarComponent} from  "./sidebar.component"
 
 @Component({
     selector: 'my-app',
     template: `
-    <h1>{{blog.name}}</h1>
-    <ul>
-        <li *ngFor="let post of posts">
-            <div [innerHTML]="post.body"></div>
-        </li>
-    </ul>
+    <div class="pure-g">
+        <sidebar class="pure-u-1-5"></sidebar>
+        <div class="pure-u-4-5">
+            <h1>{{blog.name}}</h1>
+            <ul>
+                <li *ngFor="let post of posts">
+                    <div [innerHTML]="post.body"></div>
+                </li>
+            </ul>
+        </div>
+    </div>
     `,
+    directives: [SidebarComponent],
     providers: [ConfigService, TumblrService]
 })
 export class AppComponent{
---
+++ app/sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sidebar',
+    template: `
+        <div>Sidebar</div>
+    `
+})
+export class SidebarComponent{
+
+}
