65df2d132e8908d60869f5bca9768f8499065975
Tom Z?hner <tom.zoehner@gmail.com>
2016-05-22 15:31:28 +0200

    Sidebar styling

--- app/components/sidebar.component.ts
+++ app/components/sidebar.component.ts
@@ -8,10 +8,10 @@ import {SettingsService} from "../shared/settings.service";
     template: `
         <div>
             <ul>
-                <li>
+                <li class="settings" class="settings {{selected('/settings') ? 'selected' : ''}}">
                     <a [routerLink]="['/settings']">Settings</a>
                 </li>
-                <li *ngFor="let blog of blogs">
+                <li *ngFor="let blog of blogs" class="blog {{selected('/blog/' + blog.name) ? 'selected' : ''}}">
                     <a [routerLink]="['/blog/',blog.name]">{{blog.name}}</a>
                 </li>
             </ul>
@@ -24,12 +24,41 @@ import {SettingsService} from "../shared/settings.service";
             width: 20%;
             background-color: #EEEEEE;
         }
+        
+        ul{
+            padding-left: 2em;
+            list-style: none;
+        }
+        
+        li.settings{
+            margin-bottom: 2em;
+        }
+        
+        li.blog{
+            margin-bottom: 0.5em;
+        }
+        
+        li.selected{
+            font-weight: bold;
+        }
+        
+        a:link, a:visited, a:hover, a:active {
+            color: black;
+            text-decoration: none;
+        }
     `],
     directives: [ROUTER_DIRECTIVES]
 })
 export class SidebarComponent{
-    private blogs:Blog[]
+    private blogs:Blog[];
     constructor(private _settingsService: SettingsService){
         _settingsService.getBlogs().subscribe(blogs => blogs === null ? this.blogs = [] : this.blogs = blogs);
+    }
+
+    selected(blogName: string){
+        if(location.pathname === blogName){
+            return true;
+        }
+        return false;
     }
 }
