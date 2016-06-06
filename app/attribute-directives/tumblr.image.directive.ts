225c1e362a141ac158f7bd5c66c5db631ca29bc9
Tom Z?hner <tom.zoehner@gmail.com>
2016-06-06 12:09:28 +0200

    set image height in tumblrImageDirective

--- app/attribute-directives/tumblr.image.directive.ts
+++ app/attribute-directives/tumblr.image.directive.ts
@@ -12,6 +12,12 @@ export class TumblrImageDirective{
     ngOnInit(){
         this.el.nativeElement.setAttribute("src", this.photo.alt_sizes[this.photo.alt_sizes.length - 1].url);
         this.el.nativeElement.setAttribute("srcset", this.createSrcSet());
+        this.el.nativeElement.setAttribute("height", this.getHeight(this.el.nativeElement.width))
+    }
+
+    private getHeight(width: number): string {
+        let ratio = this.photo.original_size.height / this.photo.original_size.width;
+        return Math.round(width * ratio) + "px";
     }
 
     private createSrcSet(): string {
@@ -19,7 +25,6 @@ export class TumblrImageDirective{
         this.photo.alt_sizes.forEach(picture => {
             srcset += picture.url + " " + picture.width + "w, "
         });
-        srcset.substring(0, srcset.lastIndexOf(", "));
-        return srcset;
+        return srcset.substring(0, srcset.lastIndexOf(", "));
     }
 }
