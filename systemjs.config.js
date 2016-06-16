070399eb568954e51e227d6f9491703f3a61c2e7
Tom Z?hner <tom.zoehner@gmail.com>
2016-06-16 09:15:03 +0200

    update for angular rc2

--- systemjs.config.js
+++ systemjs.config.js
@@ -33,7 +33,7 @@
     // Add package entries for angular packages
     ngPackageNames.forEach(function(pkgName) {
         // Bundled (~40 requests):
-        packages['@angular/'+pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
+        packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
 
         // Individual files (~300 requests):
         //packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
