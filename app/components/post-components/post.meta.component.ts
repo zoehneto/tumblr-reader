c48c2c9362f0c4b29efbfadd9aaa8fb263c5eff2
Tom Z?hner <tom.zoehner@gmail.com>
2016-06-19 12:23:50 +0200

    small fix for meta component

--- app/components/post-components/post.meta.component.ts
+++ app/components/post-components/post.meta.component.ts
@@ -8,7 +8,7 @@ import {TumblrLinkDirective} from "../../attribute-directives/tumblr.link.direct
     directives: [ROUTER_DIRECTIVES, TumblrLinkDirective],
     template: `
         <p *ngIf="post.source_url  || post.reblogged_from_name">
-            (<span *ngIf="post.source_url">Source: <a [tumblrLink]="post.source_url">{{post.source_title}}</a></span><span *ngIf="post.source_url">, </span><span *ngIf="post.reblogged_from_name">via <a [tumblrLink]="post.reblogged_from_url">{{post.reblogged_from_name}}</a></span>)
+            (<span *ngIf="post.source_url">Source: <a [tumblrLink]="post.source_url">{{post.source_title}}</a></span><span *ngIf="post.source_url && post.reblogged_from_name">, </span><span *ngIf="post.reblogged_from_name">via <a [tumblrLink]="post.reblogged_from_url">{{post.reblogged_from_name}}</a></span>)
         </p>
         
         <ul class="list-inline">
