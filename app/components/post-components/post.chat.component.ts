19dc9d64c9cf5ec4a7c720916f5624a82200deb2
Tom Z?hner <tom.zoehner@gmail.com>
2016-06-25 14:26:13 +0200

    Improvement for post-chat component

--- app/components/post-components/post.chat.component.ts
+++ app/components/post-components/post.chat.component.ts
@@ -4,13 +4,18 @@ import {Post} from "../../data.types";
 @Component({
     selector: 'post-chat',
     template: `
-        <p *ngFor="let message of post.dialogue">{{message.label}} {{message.phrase}}</p>
+        <p *ngFor="let message of post.dialogue"><span class="label">{{message.label}}</span> {{message.phrase}}</p>
     `,
     styles: [`
         p{
             margin: 0;
             padding: 1em 0 1em 1em;
         }
+        
+        span.label{
+            font-weight: 700;
+        }
+        
         p:nth-child(odd){
             background-color: lightgrey;
         }
