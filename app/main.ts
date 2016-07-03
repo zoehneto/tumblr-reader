e9ea04f9f28f9d2d2c97155046ea67b75d62e702
Tom Z?hner <tom.zoehner@gmail.com>
2016-07-03 17:19:12 +0200

    enable production mode

--- app/main.ts
+++ app/main.ts
@@ -3,7 +3,9 @@ import {AppComponent} from './app.component';
 import {HTTP_PROVIDERS, JSONP_PROVIDERS} from "@angular/http";
 import 'rxjs/add/operator/map';
 import {APP_ROUTER_PROVIDERS} from "./app.routes";
+import {enableProdMode} from '@angular/core';
 
+enableProdMode();
 bootstrap(AppComponent, [
     APP_ROUTER_PROVIDERS, HTTP_PROVIDERS, JSONP_PROVIDERS
 ]).catch(err => console.error(err));
