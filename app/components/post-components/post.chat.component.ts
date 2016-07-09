6eade057c0f0eb2c3ed54647c85cc5357dc218fe
Tom Z?hner <tom.zoehner@gmail.com>
2016-07-09 12:40:37 +0200

    improved styles for chat component

--- app/components/post-components/post.chat.component.ts
+++ app/components/post-components/post.chat.component.ts
@@ -8,16 +8,26 @@ import {Post} from "../../data.types";
     `,
     styles: [`
         p{
-            margin: 0;
+            margin: 0.5em 0;
             padding: 1em 0 1em 1em;
+            background-color: #eee;
+            border-radius: 4px;
+        }
+        
+        p:first-child{
+            margin-top: 0;
+        }
+        
+        p:last-child{
+            margin-bottom: 0;
+        }
+        
+        p:nth-child(odd){
+            background-color: #ddd;
         }
         
         span.label{
             font-weight: 700;
-        }
-        
-        p:nth-child(odd){
-            background-color: lightgrey;
         }
     `]
 })
