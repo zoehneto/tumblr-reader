1ff73c018fa3c7a1143fedcc86fe55534f3a06ae
Tom Z?hner <tom.zoehner@gmail.com>
2016-07-16 12:48:52 +0200

    Modifications to work with real webserver

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = webpackMerge(commonConfig, {
 
     output: {
         path: helpers.root('dist'),
-        publicPath: '/',
+        publicPath: '/tumblr-reader',
         filename: '[name].[hash].js',
         chunkFilename: '[id].[hash].chunk.js'
     },
