f057582af94cce7351b9bf9c43d6cf0a13b9ff70
Tom Z?hner <tom.zoehner@gmail.com>
2016-09-21 17:19:13 +0200

    Work around incorrect api information for post type quote

--- src/app/components/post-components/post-quote/post-quote.component.ts
+++ src/app/components/post-components/post-quote/post-quote.component.ts
@@ -1,16 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, OnChanges, DoCheck } from '@angular/core';
 import { Post } from '../../../data.types';
 
 @Component({
     selector: 'post-quote',
     template: `
         <blockquote>
-            <h2>{{post.text}}</h2>
+            <h2 #quote [innerHTML]="post.text"></h2>
         </blockquote>
         <div [innerHTML]="post.source"></div>
     `,
     styleUrls: ['./post-quote.component.scss']
 })
-export class PostQuoteComponent {
+export class PostQuoteComponent implements DoCheck {
     @Input('post') post: Post;
+    @ViewChild('quote') quote: ElementRef;
+
+    ngDoCheck() {
+        if (this.quote.nativeElement.children.length > 0) {
+            this.quote.nativeElement.classList.add('no-margin');
+        } else {
+            this.quote.nativeElement.classList.remove('no-margin');
+        }
+    }
 }
