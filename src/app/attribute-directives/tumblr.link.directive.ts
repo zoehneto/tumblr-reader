d8c23ddf6aba17fa0bf09151e1ed3665f551d7a8
Tom Z?hner <tom.zoehner@gmail.com>
2018-08-10 17:08:05 +0200

    Rewrite more links and support links to tag pages

--- src/app/attribute-directives/tumblr.link.directive.ts
+++ src/app/attribute-directives/tumblr.link.directive.ts
@@ -12,25 +12,29 @@ export class TumblrLinkDirective implements DoCheck {
         if (this.tumblrLink) {
             this.applySettings(this.el.nativeElement, this.tumblrLink);
         } else {
-            const blogLinks = this.el.nativeElement.querySelectorAll('a.tumblr_blog');
+            const blogLinks = this.el.nativeElement.querySelectorAll('a');
             if (blogLinks.length > 0) {
                 for (const blogLink of blogLinks) {
-                    if (blogLink.getAttribute('href').indexOf('tumblr') > -1) {
-                        this.applySettings(blogLink, blogLink.getAttribute('href'));
-                    }
+                    this.applySettings(blogLink, blogLink.getAttribute('href'));
                 }
             }
         }
     }
 
-    private applySettings(element: Element, link: string): void {
-        element.setAttribute('href', this.getHref(link));
+    private applySettings(element: HTMLAnchorElement, link: string): void {
+        const currentHref = element.getAttribute('href') || '';
+        const newHref = this.getHref(link);
+        if (element.innerText.trim().toLowerCase() === currentHref.trim().toLowerCase()) {
+            element.innerText = newHref;
+        }
+        element.setAttribute('href', newHref);
         element.setAttribute('target', '_blank');
     }
 
     private getHref(currentHref: string): string {
         // External link
-        if (currentHref.indexOf('tumblr.com') < 0 || currentHref.indexOf('t.umblr.com') > -1) {
+        if (!currentHref.includes('tumblr.com') || currentHref.includes('t.umblr.com')
+            || currentHref.includes('tumblr.com/follow')) {
             return currentHref;
         }
 
@@ -40,7 +44,12 @@ export class TumblrLinkDirective implements DoCheck {
         const data = currentHref.split('.tumblr.com/post/');
 
         if (data.length < 2 || data[1].match(/\d+/) == null) {
-            return '#/blog/' + currentHref.substring(0, currentHref.indexOf('.'));
+            const blogName = currentHref.substring(0, currentHref.indexOf('.'));
+            if (currentHref.includes('/tagged/')) {
+                const tag = currentHref.split('.tumblr.com/tagged/')[1].replace(/\-/g , '%20');
+                return '#/blog/' + blogName + '/tag/' + tag;
+            }
+            return '#/blog/' + blogName;
         }
         return '#/blog/' + data[0] + '/post/' + (data[1].match(/\d+/) || [])[0];
     }
--- src/app/components/post-components/post-quote/post-quote.component.ts
+++ src/app/components/post-components/post-quote/post-quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, ElementRef, OnChanges, DoCheck } from '@angular/core';
+import {Component, Input, ViewChild, ElementRef, DoCheck, ChangeDetectorRef} from '@angular/core';
 import { Post } from '../../../data.types';
 
 @Component({
@@ -7,7 +7,7 @@ import { Post } from '../../../data.types';
         <blockquote>
             <h2 #quote [innerHTML]="post.text"></h2>
         </blockquote>
-        <div [innerHTML]="post.source"></div>
+        <div [innerHTML]="post.source" tumblrLink></div>
     `,
     styleUrls: ['./post-quote.component.scss']
 })
@@ -15,11 +15,14 @@ export class PostQuoteComponent implements DoCheck {
     @Input('post') post: Post;
     @ViewChild('quote') quote: ElementRef;
 
+    constructor(private detectorRef: ChangeDetectorRef) {}
+
     ngDoCheck() {
         if (this.quote.nativeElement.children.length > 0) {
             this.quote.nativeElement.classList.add('no-margin');
         } else {
             this.quote.nativeElement.classList.remove('no-margin');
         }
+        this.detectorRef.detectChanges();
     }
 }
